5ee1047cf0e24f07fd917784e852f5a73b378294
Brock Sauvage <bsauvage14@gmail.com>
2016-12-10 22:10:50 -0600

    Started messing with music changes in ModernScene

---
+++ TestProject/Assets/scripts/MusicChange.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicChange : MonoBehaviour {
+
+	public AudioSource source;
+	public AudioClip song1;
+	public AudioClip song2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		AudioSource.PlayClipAtPoint (song2, transform.position, 0.5f);
+	}
+}
