eda247fc1614bd27fe7f99300b518c949cb7f56b
lucianofasani <lucianofasani@gmail.com>
2016-12-10 19:39:15 -0600

    Added my changes

---
+++ TestProject/Assets/scripts/Trampoline.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trampoline : MonoBehaviour {
+
+	private SixteenBitController player;
+	private Animator anim;
+
+	void Start(){
+
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<SixteenBitController>();
+		anim = gameObject.GetComponent<Animator> ();
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.CompareTag ("Player")) {
+
+			StartCoroutine (player.Bounce (.005f, 1600, player.transform.position)); //Must start Coroutine for IEnumerator
+
+		}
+
+		anim.SetTrigger ("Trip");
+
+	}
+}
