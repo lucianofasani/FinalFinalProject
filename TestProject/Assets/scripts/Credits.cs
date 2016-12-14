af0b6b739e4998af4c857b2483915a9f816024b9
Brock Sauvage <bsauvage14@gmail.com>
2016-12-13 21:10:51 -0600

    More changes made to ModernScene. Title screen finished and credit roll added

---
+++ TestProject/Assets/scripts/Credits.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	public GameObject creditRoll;
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		creditRoll.transform.Translate (new Vector3 (0f, 0.015f, 0f));
+	}
+		
+}
--- TestProject/Assets/scripts/TitleScreen.cs
+++ TestProject/Assets/scripts/TitleScreen.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour {
 
@@ -15,7 +16,11 @@ public class TitleScreen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		if(Input.GetKeyDown(KeyCode.Return))
+		{
+			SceneManager.LoadScene ("IntroScene");
+			SceneManager.UnloadScene ("Title Screen");
+		}
 	}
 
 	IEnumerator FlashText(){
